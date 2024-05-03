7e6060700fb0de51ca40487b1d77af9ff723f8a4
Nirvek <nirvekpandey@gmail.com>
2024-05-02 22:01:18 -0700

    finshed expose

--- assets/scripts/expose.js
+++ assets/scripts/expose.js
@@ -4,9 +4,9 @@ window.addEventListener('DOMContentLoaded', init);
 function init() {
   /* horn */
   const horn = document.getElementById('horn-select');
-  horn.addEventListener('change', function () {
-    const hornType = horn.value;
-    let image = document.querySelector("#expose img")
+  horn.addEventListener('change', function Horn() {
+    const hornType = horn.value; /* get the selected horn */
+    let image = document.querySelector("#expose img") /* set picture */
     image.src = `assets/images/${hornType}.svg`;
     let audio = document.getElementsByClassName('hidden');
     audio.src = `assets/audio/${hornType}.mp3`;
@@ -15,10 +15,12 @@ function init() {
 
   /* volume */
   const volume = document.getElementById('volume');
-  volume.addEventListener('change', function () {
+  volume.addEventListener('change', function Volume() {
     let volumeVal = volume.value;
     let audio = document.querySelector('audio');
     let volumeImage = document.querySelector('#volume-controls img');
+
+    /* change volume icon based on selected volume */
     if (volumeVal == 0) {
       volumeImage.src = 'assets/icons/volume-level-0.svg';
     } else if (volumeVal < 34) {
@@ -34,18 +36,17 @@ function init() {
 
   /* button */
   const button = document.querySelector('button');
-  button.addEventListener('click', function () {
+  button.addEventListener('click', function Button() {
     const audio = document.getElementsByClassName('hidden');
+    /*console.log(audio);*/
     const sound = new Audio(audio.src);
-    console.log(sound);
     sound.play();
 
     const hornType = document.getElementById('horn-select').value;
     if (hornType == 'party-horn') {
       const jsConfetti = new JSConfetti();
       jsConfetti.addConfetti();
-    } /*used chatGPT for help here*/
-
+    }
   });
 
 }
