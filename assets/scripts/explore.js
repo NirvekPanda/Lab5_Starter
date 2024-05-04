2b0bf237faa4ed5e2b4d7e0a454a52f85c9c5f6f
Nirvek <nirvekpandey@gmail.com>
2024-05-03 22:51:27 -0700

    wokred on site added bulk of code

--- assets/scripts/explore.js
+++ assets/scripts/explore.js
@@ -3,5 +3,50 @@
 window.addEventListener('DOMContentLoaded', init);
 
 function init() {
-  // TODO
+  // Initialize the speech synthesis API
+
+  const synth = window.speechSynthesis;
+  const voiceSelect = document.getElementById('voice-select');
+  // Populate the voice select dropdown on page load
+  synth.onvoiceschanged = function () {
+    let voices = synth.getVoices();
+    voices.forEach((voice) => {
+      let option = document.createElement('option');
+      option.textContent = `${voice.name} (${voice.lang})`;
+      option.setAttribute('data-lang', voice.lang);
+      option.setAttribute('data-name', voice.name);
+      voiceSelect.appendChild(option);
+    });
+  };
+
+  // Initialize the button for triggering speech
+  const button = document.querySelector('button');
+  // Handle button click
+  button.addEventListener('click', function () {
+
+    const textInput = document.getElementById('text-to-speak');
+    const textVal = textInput.value;
+    const textData = voiceSelect.selectedOptions[0].getAttribute('data-name');
+
+    let utterance = new SpeechSynthesisUtterance(textVal);
+    const selected_voice = synth.getVoices().find(voice => voice.name === textData);
+
+
+    if (selected_voice) {
+      // Initialize the image for the face
+      const image = document.querySelector('img');
+      // Change the image to open mouth while speaking
+      image.src = 'assets/images/smiling-open.png';
+
+      // Speak the text
+      utterance.voice = selected_voice;
+      synth.speak(utterance);
+
+      // Change the image back to closed mouth when finished speaking
+      utterance.onend = function () {
+        image.src = 'assets/images/smiling.png';
+      }
+    };
+  });
+
 }
